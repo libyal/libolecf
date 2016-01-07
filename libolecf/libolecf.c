66568ab863699c95cea365f758bdf2d2db421bff
Joachim Metz <joachim.metz@gmail.com>
2016-01-07 06:45:30 +0100

    Applied updates.

--- libolecf/libolecf.c
+++ libolecf/libolecf.c
@@ -1,7 +1,7 @@
 /*
  * Library to access the Object Linking and Embedding (OLE) Compound File (CF) format
  *
- * Copyright (C) 2008-2015, Joachim Metz <joachim.metz@gmail.com>
+ * Copyright (C) 2008-2016, Joachim Metz <joachim.metz@gmail.com>
  *
  * Refer to AUTHORS for acknowledgements.
  *
