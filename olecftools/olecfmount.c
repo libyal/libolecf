faabcf38b2d986a412417c0687c13a405c5b63cb
Joachim Metz <joachim.metz@gmail.com>
2023-01-25 04:58:15 +0100

    Applied updates

--- olecftools/olecfmount.c
+++ olecftools/olecfmount.c
@@ -1,7 +1,7 @@
 /*
  * Mounts an Object Linking and Embedding (OLE) Compound File (CF)
  *
- * Copyright (C) 2008-2022, Joachim Metz <joachim.metz@gmail.com>
+ * Copyright (C) 2008-2023, Joachim Metz <joachim.metz@gmail.com>
  *
  * Refer to AUTHORS for acknowledgements.
  *
