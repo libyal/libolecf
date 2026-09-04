00bad66ef879ad25db3be3eaf1c144f4fb2c9ea5
Joachim Metz <joachim.metz@gmail.com>
2026-09-04 06:07:31 +0200

    Worked on tests and changes for CI tests

--- olecftools/olecfmount.c
+++ olecftools/olecfmount.c
@@ -493,6 +493,14 @@ int main( int argc, char * const argv[] )
 
 		goto on_error;
 	}
+#if defined( HAVE_LIBFUSE3 )
+	fuse_unmount(
+	 olecfmount_fuse_handle );
+#else
+	fuse_unmount(
+	 mount_point,
+	 olecfmount_fuse_channel );
+#endif
 	fuse_destroy(
 	 olecfmount_fuse_handle );
 
